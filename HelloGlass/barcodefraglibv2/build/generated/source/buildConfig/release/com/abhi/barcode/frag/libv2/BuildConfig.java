749c4d53b9c8cc401e94d226e2383db9d06472ac
Jonassen Yuan <huddsinyuan2014@gmail.com>
2016-03-01 19:40:19 +0800

    Basic QR Code Recognization Complete.

--- HelloGlass/barcodefraglibv2/build/generated/source/buildConfig/release/com/abhi/barcode/frag/libv2/BuildConfig.java
+++ HelloGlass/barcodefraglibv2/build/generated/source/buildConfig/release/com/abhi/barcode/frag/libv2/BuildConfig.java
@@ -5,9 +5,9 @@ package com.abhi.barcode.frag.libv2;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String PACKAGE_NAME = "com.abhi.barcode.frag.libv2";
+  public static final String APPLICATION_ID = "com.abhi.barcode.frag.libv2";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "";
+  public static final String VERSION_NAME = "1.0";
 }
--- HelloGlass/barcodefraglibv2/build/generated/source/r/release/com/abhi/barcode/frag/libv2/R.java
+++ HelloGlass/barcodefraglibv2/build/generated/source/r/release/com/abhi/barcode/frag/libv2/R.java
@@ -15,8 +15,8 @@ public final class R {
         public static int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static int ic_launcher=0x7f020000;
-        public static int ic_stadium=0x7f020001;
+        public static int ic_glass_logo=0x7f020000;
+        public static int ic_launcher=0x7f020001;
     }
     public static final class id {
         public static int action_settings=0x7f080003;
@@ -31,22 +31,22 @@ public final class R {
         public static int barcode_fragment_example=0x7f070000;
     }
     public static final class string {
-        public static int action_settings=0x7f050000;
-        public static int app_name=0x7f050001;
-        public static int hello_world=0x7f050002;
+        public static int action_settings=0x7f060000;
+        public static int app_name=0x7f060001;
+        public static int hello_world=0x7f060002;
     }
     public static final class style {
-        /** 
+        /**  API 11 theme customizations can go here. 
+ API 14 theme customizations can go here. 
+
             Theme customizations available in newer API levels can go in
             res/values-vXX/styles.xml, while customizations related to
             backward-compatibility can go here.
         
- API 11 theme customizations can go here. 
- API 14 theme customizations can go here. 
          */
-        public static int AppBaseTheme=0x7f060000;
+        public static int AppBaseTheme=0x7f050000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static int AppTheme=0x7f060001;
+        public static int AppTheme=0x7f050001;
     }
 }
